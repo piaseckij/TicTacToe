3f4f4916fc2432d0f59513d704fe8fe193d054f1
piaseckij <91951283+piaseckij@users.noreply.github.com>
2021-12-04 16:45:01 +0100

    Flip (#2)
    * almost done
    
    * almost ready
    
    * final commit

--- KolkoKrzyzyk/programLogic.cs
+++ KolkoKrzyzyk/programLogic.cs
@@ -1,84 +1,8 @@
 ﻿using System;
-using System.Linq;
+
 
 namespace KolkoKrzyzyk
 {
-    enum Pola
-    {
-        empty,
-        circle,
-        cross
-    }
-
-    enum axisX
-    {
-        a,
-        b,
-        c
-    }
-
-    class dataHandling
-    {
-        string input;
-
-        private bool validateData(string input)
-        {
-
-            char[] allowedX = { 'a', 'b', 'c' };
-
-            if (input.Length > 2 || input.Length < 2)
-            {
-                Console.WriteLine("Błąd argument wejśćiowy zbyt dług/ krótki");
-                return false;   
-            }
-
-
-
-            if (allowedX.Contains(char.ToLower(input[0])))
-            {
-                return true;
-            }
-
-            return false;
-        }
-
-        public (int, int) receivePrepareData(Pola pola)
-        {
-
-            (int, int) output = (3, 3);
-
-            Console.WriteLine($"\nPodaj miejsce w którym chcesz ustawić znak {pola} \n");
-            input = Console.ReadLine();
-
-
-            if (!validateData(input))
-            {
-                return (3, 3);
-            }
-
-
-            
-            switch (char.ToLower(input[0]))
-            {
-                case 'a':
-                    output = (0, (input[1] - '0') - 1);
-                    break;
-                case 'b':
-                    output = (1, (input[1] - '0') - 1);
-                    break;
-                case 'c':
-                    output = (2, (input[1] - '0') - 1);
-                    break;
-
-
-            }
-
-            return output;
-
-        }
-    }
-
-    
 
     class application
     {
@@ -93,28 +17,32 @@ namespace KolkoKrzyzyk
             data = new dataHandling();
 
 
-
-
             tab.generateTable();
+
             do
             {
+                
+                tab.showTable();
+                tab.putSign(Pola.circle);
                 Console.Clear();
                 tab.showTable();
-                tab.putCross();
+                if (tab.checkWin() !=null)
+                    break;
+                tab.putSign(Pola.cross);
                 Console.Clear();
-                tab.showTable();
-                tab.putCircle();
-                Console.Clear();
-                tab.showTable();
-                tab.putCross();
-            } while (tab.checkWin());
 
+            } while (tab.checkWin()==null);
 
-
+            if (tab.checkWin() == Pola.empty)
+            {
+                Console.WriteLine("\nRemis");
+            }
+            else
+            {
+                Console.WriteLine($"Wygrał gracz {tab.checkWin()}");
+            }
 
         }
-
-
 
     }
 
---
+++ KolkoKrzyzyk/tableGenerationcs.cs
@@ -0,0 +1,153 @@
+﻿using System;
+
+namespace KolkoKrzyzyk
+{
+    enum Pola
+    {
+        empty,
+        circle,
+        cross
+    }
+
+    class tableGeneration
+    {
+
+        dataHandling data;
+
+
+        Pola[] table = new Pola[9];
+
+        public void generateTable()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                table[i] = Pola.empty;
+
+            }
+        }
+
+        public void showTable()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                switch (table[i])
+                {
+                    case Pola.empty:
+                        if (i != 2 && i != 5 && i != 8)
+                        {
+                            Console.Write("   " + "|");
+                        }
+                        else if (i == 2 || i == 5 || i == 8)
+                        {
+                            Console.Write("   ");
+                        }
+
+                        break;
+
+
+                    case Pola.circle:
+                        if (i != 2 && i != 5 && i != 8)
+                            Console.Write(" o" + " |");
+                        else if (i == 2 || i == 5 || i == 8)
+                            Console.Write(" o");
+                        break;
+                    case Pola.cross:
+                        if (i != 2 && i != 5 && i != 8)
+                            Console.Write(" x" + " |");
+                        else if (i == 2 || i == 5 || i == 8)
+                            Console.Write(" x");
+                        break;
+                }
+
+                if (i == 2 || i == 5)
+                {
+                    Console.WriteLine("\n-----------");
+                }
+
+            }
+        }
+
+        public void putSign(Pola pola)
+        {
+            dataHandling data = new dataHandling();
+
+            int x;
+
+            x = data.receivePrepareData(pola);
+
+            if (pola == Pola.cross && x != 200 && table[x] == Pola.empty)
+            {
+                table[x] = Pola.cross;
+            }
+            else if (pola == Pola.circle && x != 200 && table[x] == Pola.empty)
+            {
+                table[x] = Pola.circle;
+            }
+
+        }
+
+        public Pola? checkWin()
+        {
+
+            #region sprawdzenie wygranej poziom
+            if (table[0] == table[1] && table[1] == table[2] && table[0] != Pola.empty)
+            {
+                return table[0];
+            }
+
+            else if (table[3] == table[4] && table[4] == table[5] && table[3] != Pola.empty)
+            {
+                return table[3];
+            }
+
+            else if (table[6] == table[7] && table[7] == table[8] && table[6] != Pola.empty)
+            {
+                
+                return table[6];
+            }
+            #endregion
+
+            #region sprawdzenie wygranej pion
+            else if (table[0] == table[3] && table[3] == table[6] && table[0] != Pola.empty)
+            {
+                
+                return table[0];
+            }
+            else if (table[1] == table[4] && table[4] == table[7] && table[1] != Pola.empty)
+            {
+                
+                return table[1];
+            }
+            else if (table[2] == table[5] && table[5] == table[8] && table[2] != Pola.empty)
+            {
+                return table[2];
+            }
+            #endregion
+
+            #region sprawdzanie wygranej skos
+            else if (table[0] == table[4] && table[4] == table[8]&&table[0]!=Pola.empty)
+            {
+                return table[0];
+            }
+            else if (table[2] == table[4] && table[4] == table[6]&&table[2]!=Pola.empty)
+            {
+                return table[2];
+            }
+            #endregion
+
+            #region sprawdzanie remisu
+            else if (table[0] != Pola.empty && table[1] != Pola.empty && table[2] != Pola.empty && table[3] != Pola.empty
+                && table[4] != Pola.empty && table[5] != Pola.empty && table[6] != Pola.empty && table[7] != Pola.empty
+                && table[8] != Pola.empty)
+            {
+                
+                return Pola.empty;
+            }
+
+            return null;
+           
+            #endregion
+
+        }
+    }
+}
